126cf6409dd73e1719345f46dbd7956cd13614e0
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-16 19:11:54 +0200

    finished home page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .models import Consultation, Booking
 
 @login_required(login_url='/auth/login/')
 def home(request):
-    return render(request, 'home.html', { 'user': request.user })
+    consultations = Consultation.objects.all()
+
+    return render(request, 'home.html', { 'user': request.user, 'consultations': consultations })
+
+@login_required(login_url='/auth/login/')
+def signout(request):
+    logout(request)
+    return redirect('login')
