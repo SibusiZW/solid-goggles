16ab37d04fda099badb289b1191b9a9c80d68131
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-16 19:29:54 +0200

    added cconsultation date

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ class Consultation(models.Model):
     teacher_name = models.CharField(unique=True, max_length=100)
     subject = models.CharField(max_length=100)
     class_name = models.CharField(max_length=50)
+    date = models.DateTimeField()
 
     def __str__(self):
         return f"{self.teacher_name} - {self.class_name}"
