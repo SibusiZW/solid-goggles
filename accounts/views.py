d331966e3a59cce10988490bc2dcebff3410ff0e
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-16 17:35:53 +0200

    added login logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
 def login_view(request):
     if request.method == 'POST':
-        form = LoginForm(request)
+        form = LoginForm(request.POST)
 
-        student_id = form.cleaned_data['student_id']
-        password = form.cleaned_data['password']
+        if form.is_valid():
+            student_id = form.cleaned_data['student_id']
+            password = form.cleaned_data['password']
+
+        userr = authenticate(request, student_id=student_id, password=password)
+
+        if userr:
+            login(request, userr)
+            return HttpResponse('Logged In')
+        else:
+            return HttpResponse('Incorrect credentials')
     else:
         form = LoginForm()
 
