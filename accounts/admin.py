26483ba9bac49fa070865d1f7d555ef8dbdae068
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-16 18:24:55 +0200

    added password hashing for non-staff users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    # Add your custom fields to the admin forms
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('student_id', 'student_class')}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {'fields': ('student_id', 'student_class')}),
+    )
+    # Ensure the admin displays your custom USERNAME_FIELD
+    ordering = ('student_id',)
+    list_display = ('student_id', 'email', 'is_staff')
+
+admin.site.register(User, UserAdmin)
