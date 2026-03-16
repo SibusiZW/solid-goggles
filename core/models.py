f877a723114a25099876ca103398dd7813cfce5b
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-16 17:57:17 +0200

    added consultation model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Consultation(models.Model):
+    teacher_name = models.CharField(unique=True, max_length=100)
+    subject = models.CharField(max_length=100)
+    class_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.teacher_name} - {self.class_name}"
+
+class Booking(models.Model):
+    pass
